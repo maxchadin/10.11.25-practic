98b10f30b0d1dcf0c4c6b58bd7220611ec9263b6
maxchadin <maxchadin080418@gmail.com>
2025-11-10 13:30:29 +0300

    commit 4

--- main1.cpp
+++ main1.cpp
@@ -2,21 +2,44 @@
 
 struct IntArray{
   void add(int i);
+  int at(size_t id) const noexcept;
   int get(size_t id) const;
-  size_t size() const;
-  int last() const;
+  size_t size() const noexcept;
+  int last() const noexcept;
   IntArray(int i);
   ~IntArray();
-  int*a
+  int*a;
   size_t k;
+  
   IntArray::~IntArray(){
     delete[] a;
   }
-  IntArray::IntArray(int i){
-    a()new int [1]),
+  IntArray::IntArray(int i)
+    a(new int [1]),
     k(1)
   {
-    a[o]=i;
+    *a=i;
+  }
+  int IntArray::get(size_t id) const noexcept{
+    return a[id];
+  }
+  
+  size_t IntArray::size() const noexcept{
+    return k;
+  }
+  
+  int IntArray::last() const noexcept{
+    return get(size()-1);
+  }
+  
+  void IntArray::add(int i){
+    int * tmp = new int[size()+1];
+    for(size_t i=0;i<size();++i){
+      tmp[i] = get(i);
+    }
+    delete[] a;
+    a=tmp;
+    ++k;
   }
 }
 
