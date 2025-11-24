ff324edcc8add6b7248de37c6745b54f87c786b9
maxchadin <maxchadin080418@gmail.com>
2025-11-24 16:38:35 +0300

    final commit with homework

--- main1.cpp
+++ main1.cpp
@@ -1,86 +1,217 @@
 #include <iostream>
+#include <fstream>
 
 struct IntArray {
-    void add(int i);
-    int at(size_t id) const noexcept;
-    int get(size_t id) const noexcept;
-    size_t size() const noexcept;
-    int last() const noexcept;
-    IntArray(int i);
-    ~IntArray();
+  IntArray(int i);
+  ~IntArray();
+  void add(int i);
+  int get(size_t id) const noexcept;
+  size_t size() const noexcept;
+  int last() const noexcept;
 
-    int* a;
-    size_t k;
+  int* a;
+  size_t k;
 };
 
 IntArray::~IntArray() {
-    delete[] a;
+  delete[] a;
 }
 
 IntArray::IntArray(int i)
-    : a(new int[1]),
+  : a(new (std::nothrow) int[1]),
     k(1)
 {
-    *a = i;
-}
-
-int IntArray::get(size_t id) const noexcept {
-    return a[id];
-}
-
-int IntArray::at(size_t id) const noexcept {
-    return a[id];
-}
-
-size_t IntArray::size() const noexcept {
-    return k;
-}
-
-int IntArray::last() const noexcept {
-    return get(size() - 1);
+  if (!a) {
+    return;
+  }
+  *a = i;
 }
 
 void IntArray::add(int i) {
-    int* tmp = new int[size() + 1];
-    for (size_t j = 0; j < size(); ++j) {
-        tmp[j] = get(j);
-    }
-    tmp[size()] = i;
-    delete[] a;
-    a = tmp;
-    ++k;
+  size_t old_size = size();
+  int* tmp = new (std::nothrow) int[old_size + 1];
+  if (!tmp) return;
+
+  for (size_t j = 0; j < old_size; ++j) {
+    tmp[j] = get(j);
+  }
+  tmp[old_size] = i;
+
+  delete[] a;
+  a = tmp;
+  ++k;
 }
 
-int main() {
-    int next = 0;
-    if (!(std::cin >> next)) {
+int IntArray::get(size_t id) const noexcept {
+  return a[id];
+}
+
+size_t IntArray::size() const noexcept {
+  return k;
+}
+
+int IntArray::last() const noexcept {
+  return get(size() - 1);
+}
+
+class IntMatrix : public IntArray {
+public:
+  IntMatrix(size_t rows, size_t cols)
+    : IntArray(0), rows_(rows), cols_(cols)
+  {
+    a = new (std::nothrow) int[rows * cols];
+    k = rows * cols;
+    if (a) {
+      for (size_t i = 0; i < k; ++i)
+        a[i] = 0;
+    }
+  }
+
+  ~IntMatrix() override {
+  }
+
+  size_t getRows() const noexcept { return rows_; }
+  size_t getCols() const noexcept { return cols_; }
+
+  int get(size_t r, size_t c) const noexcept {
+    return a[r * cols_ + c];
+  }
+
+  void set(size_t r, size_t c, int v) noexcept {
+    a[r * cols_ + c] = v;
+  }
+
+  bool addRowAfter(size_t row_after, int fill_val) {
+    if (row_after > rows_) return false;
+
+    size_t new_rows = rows_ + 1;
+    size_t new_size = new_rows * cols_;
+    int* new_a = new (std::nothrow) int[new_size];
+    if (!new_a) return false;
+
+    for (size_t r = 0; r < rows_; ++r) {
+      for (size_t c = 0; c < cols_; ++c) {
+        size_t src_idx = r * cols_ + c;
+        size_t dst_r = (r < row_after) ? r : r + 1;
+        size_t dst_idx = dst_r * cols_ + c;
+        new_a[dst_idx] = a[src_idx];
+      }
+    }
+
+    for (size_t c = 0; c < cols_; ++c) {
+      new_a[row_after * cols_ + c] = fill_val;
+    }
+
+    delete[] a;
+    a = new_a;
+    k = new_size;
+    rows_ = new_rows;
+    return true;
+  }
+
+  bool addRowAndColAfter(size_t row_after, size_t col_after) {
+    if (row_after > rows_ || col_after > cols_) return false;
+
+    size_t new_rows = rows_ + 1;
+    size_t new_cols = cols_ + 1;
+    size_t new_size = new_rows * new_cols;
+    int* new_a = new (std::nothrow) int[new_size];
+    if (!new_a) return false;
+
+    for (size_t i = 0; i < new_size; ++i)
+      new_a[i] = 0;
+
+    for (size_t r = 0; r < rows_; ++r) {
+      for (size_t c = 0; c < cols_; ++c) {
+        size_t src = r * cols_ + c;
+        size_t dst_r = (r < row_after) ? r : r + 1;
+        size_t dst_c = (c < col_after) ? c : c + 1;
+        size_t dst = dst_r * new_cols + dst_c;
+        new_a[dst] = a[src];
+      }
+    }
+
+    delete[] a;
+    a = new_a;
+    k = new_size;
+    rows_ = new_rows;
+    cols_ = new_cols;
+    return true;
+  }
+
+  void print() const {
+    for (size_t r = 0; r < rows_; ++r) {
+      for (size_t c = 0; c < cols_; ++c) {
+        std::cout << get(r, c);
+        if (c + 1 < cols_) std::cout << ' ';
+      }
+      std::cout << '\n';
+    }
+  }
+
+private:
+  size_t rows_;
+  size_t cols_;
+};
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    return 1;
+  }
+
+  char* filename = argv[1];
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    return 1;
+  }
+
+  size_t rows, cols;
+  if (!(file >> rows >> cols)) {
+    file.close();
+    return 1;
+  }
+
+  IntMatrix m(rows, cols);
+  if (!m.a) {
+    file.close();
+    return 2;
+  }
+
+  for (size_t r = 0; r < rows; ++r) {
+    for (size_t c = 0; c < cols; ++c) {
+      int val;
+      if (!(file >> val)) {
+        file.close();
         return 1;
+      }
+      m.set(r, c, val);
+    }
+  }
+  file.close();
+
+  int cmd, p1, p2;
+  while (std::cin >> cmd >> p1 >> p2) {
+    bool ok = false;
+    if (cmd == 1) {
+      size_t row_after = static_cast<size_t>(p1);
+      ok = m.addRowAfter(row_after, p2);
+    } else if (cmd == 3) {
+      size_t row_after = static_cast<size_t>(p1);
+      size_t col_after = static_cast<size_t>(p2);
+      ok = m.addRowAndColAfter(row_after, col_after);
+    } else {
+      return 3;
     }
 
-    try {
-        IntArray a(next);
-        while (std::cin >> next) {
-            a.add(next);
-        }
+    if (!ok) return 3;
 
-        if (std::cin.fail() && !std::cin.eof()) {
-            return 1;
-        }
+    m.print();
+    std::cout << '\n';
+  }
 
-        int last_num = a.last();
-        size_t count = 0;
-        for (size_t i = 0; i < a.size() - 1; ++i) {
-            int d = a.get(i);
-            if (last_num != 0 && d % last_num == 0) {
-                ++count;
-            }
-        }
+  if (std::cin.fail() && !std::cin.eof()) {
+    return 1;
+  }
 
-        std::cout << count << "\n";
-    }
-    catch (const std::bad_alloc&) {
-        return 2;
-    }
-
-    return 0;
+  return 0;
 }
